ee138e49a4500bfc9c6973305ef9517a6e8bb10b
Anil <rawatanil488@gmail.com>
2019-06-22 19:22:45 +0530

    implemented jwt token and login user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../../models/User')
@@ -14,13 +16,21 @@ router.post('/',
     check('name', 'Name is required')
       .not()
       .isEmpty(),
-    check('email', 'Please include a valid email').isEmail(),
-    check('password', 'Please enter a password with 6 or more charcters').isLength({ min:6 })
+    check(
+      'email',
+      'Please include a valid email'
+    ).isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more charcters'
+    ).isLength({ min:6 })
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
-      return res.status(400).json({ errors: errors.array() });
+      return res
+        .status(400)
+        .json({ errors: errors.array() });
     }
 
     const { name, email, password } = req.body;
@@ -53,7 +63,22 @@ router.post('/',
         await user.save();
 
         // Return jsonwwebtoken
-        res.send('User Registered');
+        const payload = {
+          user: {
+            id: user.id
+          }
+        }
+
+        jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          { expiresIn: 360000 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({ token });
+          }
+        );
+
     } catch (error) {
         console.log(error.message);
         res.status(500).send('Server error ');
