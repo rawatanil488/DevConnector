72af1ef4eb97676dfed219655d711b32b100c056
Anil <rawatanil488@gmail.com>
2019-06-23 11:13:54 +0530

    get profile by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -104,4 +104,38 @@ router.post('/',
   }
 )
 
+// @route   Get api/profile
+// @desc    Get all profiles
+// @access  Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.send(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+})
+
+// @route   Get api/profile/user/:user_id
+// @desc    Get profile by User Id
+// @access  Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profiles = await Profile.findOne({ user: req.params.user_id }).populate('user', ['name', 'avatar']);
+    
+    if (!profile) return res.status(400).json({ msg: 'There is no profile for this user' });
+
+    res.send(profiles);
+  } catch (err) {
+    console.error(err.message);
+    
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.status(500).send('Server Error');
+  }
+})
+
 module.exports = router;
