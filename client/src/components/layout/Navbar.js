6ecabf3e264ddf8825f76cc688869e5db9e4e495
Anil <rawatanil488@gmail.com>
2019-06-26 12:11:03 +0530

    created spinner  and started working on Dashboard for the users

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,10 +8,18 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
-        <Link onClick={logout} to="/">
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{'  '}
+          <span className='hide-sm'>
+            Dashboard
+          </span>
+        </Link>
+      </li>
+      <li>
+        <a onClick={logout} href="#!">
           <i className="fas fa-sign-out-alt"></i>{'  '}
           <span className="hide-sm">Logout</span>
-        </Link>
+        </a>
       </li>
     </ul>
   );
